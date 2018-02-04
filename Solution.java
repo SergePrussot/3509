6de60862c6ab69fb25f11750262440eb32498dc0
Serge Prusov <serge.prusov@gmail.com>
2018-02-04 18:48:32 +0100

    Animals initial commit

---
+++ Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task35.task3509;
+
+import java.util.*;
+
+
+/* 
+Collections & Generics
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static <T> ArrayList<T> newArrayList(T... elements) {
+        ArrayList<T> list = new ArrayList<>();
+        for (int i = 0; i < elements.length; i++) {
+            list.add(elements[i]);
+        }
+        return list;
+    }
+
+    public static <T> HashSet<T> newHashSet(T... elements) {
+        HashSet<T> set = new HashSet<>();
+        for (int i = 0; i < elements.length; i++) {
+            set.add(elements[i]);
+        }
+        return set;
+    }
+
+    public static <K,V> HashMap<K, V> newHashMap(List<? extends K> keys, List<? extends V> values) {
+        if (keys.size() != values.size()){
+            throw new IllegalArgumentException();
+        }
+        HashMap<K, V> map = new HashMap<>();
+        for (int i = 0; i < keys.size(); i++) {
+            map.put(keys.get(i), values.get(i));
+        }
+        return map;
+    }
+}
